b3c139e829e3f9681f5b33a18c61d35044538521
ThisDevDoesNotExist <thisdevdoesnotexist.fox099@passmail.net>
2025-01-17 09:44:08 +0100

    increase the volume of fluid_box to allow higher flow rate

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -7,7 +7,10 @@ local pumpTypes = { "pump", "offshore-pump" }
 for _, typeName in pairs(pumpTypes) do
     for itemName, _ in pairs(data.raw[typeName]) do
         if data.raw[typeName][itemName].pumping_speed then
-            data.raw[typeName][itemName].pumping_speed = math.ceil(data.raw[typeName][itemName].pumping_speed * pump_speed_multiplier)
+            data.raw[typeName][itemName].pumping_speed = math.ceil(data.raw[typeName][itemName].pumping_speed *
+                pump_speed_multiplier)
+            data.raw[typeName][itemName].fluid_box.volume = math.ceil(data.raw[typeName][itemName].fluid_box.volume *
+                pump_speed_multiplier * 2.5)
         end
     end
 end
